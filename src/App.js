fb193b4f17f559871538e651917bdc8db2eb306e
afrin <mohdafrinnoushad@gmail.com>
2024-02-19 20:07:45 +0530

    rout changed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/booking-react-app' element={<Home/>}/>
+     
       <Route path='/hotels' element={<List/>}/>
       <Route path='/hotels/:id' element={<Hotel/>}/>
     </Routes>
